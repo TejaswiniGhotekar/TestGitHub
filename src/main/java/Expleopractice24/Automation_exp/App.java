9491d85d69360eaa50313bf7f62e4f147b8c60cc
tshivajighoteka <tshivajighoteka@L-INE-2P1LLW2.ardianet.net>
2024-01-23 17:19:56 +0530

    Commiting first code

---
+++ src/main/java/Expleopractice24/Automation_exp/App.java
@@ -0,0 +1,13 @@
+package Expleopractice24.Automation_exp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Expleopractice24/Automation_exp/testClass.java
@@ -0,0 +1,13 @@
+package Expleopractice24.Automation_exp;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		int a = 10;
+		int b = 8;
+		System.out.println(a+b);
+
+	}
+
+}

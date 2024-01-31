0721566aa179c3e68c879de6d0a8bf10617de52d
pmadhavraojayap <pmadhavraojayap@ESLLAP-4276.ardianet.net>
2024-01-31 13:51:53 +0530

    Commited

---
+++ src/test/java/Expleopractice24/Automation_exp/Test.java
@@ -0,0 +1,31 @@
+package Expleopractice24.Automation_exp;
+
+
+public class Test {
+
+
+
+	static int z = 5;  //Static variable
+	int y = 1;   //Global variable
+	
+	public static void a()
+	{
+		z = 10;
+		System.out.println(z);
+	}
+	public void b()
+	{
+		z = 15;
+		System.out.println(z);
+		a();
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(z);
+		a();
+		Test C5 = new Test ();
+		C5.b();
+	}
+}
+
+

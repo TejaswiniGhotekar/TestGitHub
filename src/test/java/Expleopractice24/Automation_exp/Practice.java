99a06f2224ed3d9bf86a62012e212c2223be13be
9876 <9876@L-INE-1YL4PL3.ardianet.net>
2024-01-31 11:34:10 +0530

    commiting code

---
+++ src/test/java/Expleopractice24/Automation_exp/Practice.java
@@ -0,0 +1,20 @@
+package Expleopractice24.Automation_exp;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int a = 12;
+		int b = 15;
+
+		System.out.println(a+b);
+
+		int c = 23, d = 25; 
+
+		
+		
+	}
+
+}
